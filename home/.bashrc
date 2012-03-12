6283bd6b664802609ce04fe6de2998ae04a4f4ab
Tony Wooster <twooster@gmail.com>
2012-03-12 14:19:58 -0700

    Updated for rvm

--- home/.bashrc
+++ home/.bashrc
@@ -214,6 +214,7 @@ fi
 # FINALIZE SETUP
 #-------------------------------------------------------------------------------
 
-[[ -s "~/.rvm/scripts/rvm" ]] && source "~/.rvm/scripts/rvm"  # This loads RVM into a shell session.
+#PATH=$HOME/.rvm/bin:$PATH # Add RVM to PATH for scripting
 
 . ~/.prompt.bash
+
--- reverse-install.sh
+++ reverse-install.sh
@@ -7,6 +7,9 @@
 #   -r    raw mode, ignoring backslashes
 while IFS= read -r file; do
     if [[ -f "$HOME/$file" && "$HOME/$file" -nt "$prefix/$file" ]]; then
+        echo "Copying newer file: $file"
         cp "$HOME/$file" "$prefix/$file"
+    else
+        echo "Skipping older file: $file"
     fi
 done < to-be-installed
