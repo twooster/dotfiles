e07d9ffd578e3d74538f0898c05fd105b6dc3dc4
Tony Wooster <tony.wooster@causes.com>
2012-09-20 11:53:18 -0700

    Added new git aliases

--- dotlink/bash-sources/aliases.sh
+++ dotlink/bash-sources/aliases.sh
@@ -8,15 +8,18 @@ alias l.="ls -d .*"
 alias la="ls -a"
 alias reload=". ~/.bashrc"
 alias vimrc="vim ~/.vimrc"
+alias causes="cd ~/causes"
 
 [ -n `command -v git` ] && {
+    alias g='git'
     alias gs='git status'
     alias gap='git add -p'
-    alias gai='git add -i'
     alias gau='git add -u'
     alias gco='git checkout'
     alias gcm='git commit -m'
     alias gol='git log --graph --oneline --decorate'
+    alias gca='git commit --amend'
+    alias gsu='git submodule update --init'
 }
 
 servedir() {
