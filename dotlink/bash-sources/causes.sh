fe798bcd6543f8feb55a3298ca581c6688aab5dd
Tony Wooster <tony.wooster@causes.com>
2012-12-04 00:09:45 -0800

    Add causes-specific skip-checks alias

--- dotlink/bash-sources/causes.sh
+++ dotlink/bash-sources/causes.sh
@@ -1,5 +1,9 @@
 alias cdc="cd ~/causes"
 
+function sc() {
+    eval "SKIP_CHECKS=test_history $@"
+}
+
 function prodcon() {
     local server="${1:-cauapp01}"
     ssh -t "dev@${server}" prodcon causes $2
