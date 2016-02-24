2b230ae72497b48f6200ab6872eebc768cf748d6
Tony Wooster <twooster@gmail.com>
2016-02-24 12:23:06 +0100

    Update virtualenv stuff for py3

--- autolink/bash-sources/virtualenv.sh
+++ autolink/bash-sources/virtualenv.sh
@@ -2,12 +2,38 @@
 # VIRTUALENV STUFF
 #-------------------------------------------------------------------------------
 if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+  PY3=`which python3`
+  MODARGS=""
+  if [ ! -z "$PY3" ]; then
+    if "$PY3" -m venv -h &> /dev/null; then
+      PYTHON="$PY3"
+      MODULE=venv
+    elif "$PY3" -m virtualenv -h &> /dev/null; then
+      PYTHON="$PY3"
+      MODULE=virtualenv
+    fi
+  fi
+  if [ -z "$PYTHON" ]; then
+    PY2=`which python`
+    if "$PY2" -m virtualenv -h &> /dev/null; then
+      PYTHON="$PY2"
+      MODULE=virtualenv
+      MODARGS="--no-site-packages"
+    fi
+  fi
+
+  if [ ! -z "$PYTHON" ]; then
     export WORKON_HOME=~/.virtualenvs
-    export VIRTUALENVWRAPPER_PYTHON=`which python`
-    export VIRTUALENVWRAPPER_VIRTUALENV=`which python`
-    export VIRTUALENVWRAPPER_VIRTUALENV_ARGS="-m virtualenv --no-site-packages"
+    export VIRTUALENVWRAPPER_PYTHON="$PYTHON"
+    export VIRTUALENVWRAPPER_VIRTUALENV="$PYTHON"
+    export VIRTUALENVWRAPPER_VIRTUALENV_ARGS="-m $MODULE $MODARGS"
+
+    if [ -z "${PROJECT_HOME+x}" ]; then
+      export PROJECT_HOME="$HOME/projects"
+    fi
     . /usr/local/bin/virtualenvwrapper.sh
 
     alias wo="workon"
     alias woff="deactivate"
+  fi
 fi
