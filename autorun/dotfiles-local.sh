f9d3f3f6565878d93a50919ce3b38f01187e183f
Tony Wooster <twooster@gmail.com>
2016-06-05 09:40:30 -0400

    Rework install script

---
+++ autorun/dotfiles-local.sh
@@ -0,0 +1,2 @@
+echo "export DOTFILES_BASE=\"$DOTFILES_DIR\" DOTFILES=\"$AUTOLINK_DIR\"" \
+  > "${AUTOLINK_DIR}/bash-sources/dotfiles-local-GENERATED.sh"
--- install.sh
+++ install.sh
@@ -7,67 +7,41 @@ else
     LINK_FLAGS=-ns
 fi
 
-TARGET="$HOME"
-DOTFILES_DIR="$PWD"
-AUTOLINK_DIR="$DOTFILES_DIR/autolink"
-AUTOLINK=1
-SUBMODULE_UPDATE=1
-FORCE=
+export DOTFILES_TARGET="$HOME"
+export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export AUTOLINK_DIR="$DOTFILES_DIR/autolink"
+export AUTORUN_DIR="$DOTFILES_DIR/autorun"
+
 VERBOSE=
 
-main()
+autolink_all()
 {
-    while getopts "AaMmfvt:" OPTION
-    do
-        case $OPTION in
-            A   ) AUTOLINK=;;
-            a   ) AUTOLINK=1;;
-            M   ) SUBMODULE_UPDATE=;;
-            m   ) SUBMODULE_UPDATE=1;;
-            L   ) NO_LOCAL_FILE=1;;
-            S   ) NO_SOURCE_BASHRC=1;;
-            f   ) FORCE=1;;
-            v   ) VERBOSE=1;;
-            t   ) TARGET="$OPTARG";;
-            *   ) fatal Unknown option chosen;;
-        esac
-    done
-    [ -n "$AUTOLINK" ] && autolink
-    [ -n "$SUBMODULE_UPDATE" ] && submodule_update
-    [ -z "$NO_LOCAL_FILE" ] && install_dotfiles_local_sh
-    [ -z "$NO_SOURCE_BASHRC" ] && . ~/.bashrc
-}
-
-autolink()
-{
-    autolink_files_in "$AUTOLINK_DIR"
-}
-
-submodule_update()
-{
-    git submodule update --init
-}
-
-autolink_files_in()
-{
-    for file in $( find "$1" -maxdepth 1 -mindepth 1 ); do
-        autolink_file ${file}
+    for file in $( find "$AUTOLINK_DIR" -maxdepth 1 -mindepth 1 ); do
+        link "${file}" ".${file##*/}"
     done
 }
 
-autolink_file()
+autorun_all()
 {
-    link "$1" ".${1##*/}"
+    for file in $( find "$AUTORUN_DIR" -mindepth 1 -executable ); do
+        debug Running autorun file $file
+        ${file}
+    done
 }
 
-warn()
+debug()
 {
-    echo "$*" >&2
+    [ -z "$VERBOSE" ] || echo +++ "$@"
+}
+
+info()
+{
+    echo --- "$@"
 }
 
 fatal()
 {
-    warn $@
+    echo XXX FATAL ERROR: "$@" &>2
     exit 1
 }
 
@@ -77,39 +51,51 @@ backup_rename()
     while [[ -e "$1~$i" ]]; do
         let i=i+1
     done
-    echo Renaming existing file $1 to $1~$i...
+    debug Renaming existing file $1 to $1~$i...
     mv "$1" "$1~$i" || fatal Unable to rename $1 to $1~$i
 }
 
 link()
 {
-    local flags="$LINK_FLAGS"
+    debug Linking $1 to $2
     local source="$1"
-    local target="$TARGET/${2-$1}"
-    [ -n "$VERBOSE" ] && flags="$flags -v"
+    local target="$DOTFILES_TARGET/${2-$1}"
     # Note we have to test for symlink in case the symlink is dead
-    if [[ -e "$target" || -h "$target" ]]; then
-        if [ -n "$FORCE" ]; then
-            flags="$flags -f"
-        elif [ -h "$target" ]; then
+    if [ -e "$target" -o -h "$target" ]; then
+        if [ -h "$target" ]; then
             # Symbolic link, so...
             local rl=$( readlink "$target" )
             if [[ "$rl" == "$AUTOLINK_DIR"* ]]; then
                 # Kill an existing link to a dotfile
-                rm "$target" && [ -n "$VERBOSE" ] && echo Removed existing autolink link to $target
+                debug Killing Existing autolink for $target
+                rm "$target" || warn Removal failed, skipping
             else
-                backup_rename "$target"
+                info Backing up existing link for $target
+                backup_rename "$target" || warn Backup failed, skipping
             fi
         else
-            backup_rename "$target"
+            info Backing up existing file for $target
+            backup_rename "$target" || warn Backup failed, skipping
         fi
     fi
-    ln $flags "$source" "$target"
+    if [ ! -f "$target" ]; then
+        ln $LINK_FLAGS "$source" "$target" || warn Unable to link $1 to $2
+    fi
 }
 
-install_dotfiles_local_sh()
-{
-    echo "export DOTFILES_BASE=\"$DOTFILES_DIR\" DOTFILES=\"$AUTOLINK_DIR\"" > "${AUTOLINK_DIR}/bash-sources/dotfiles-local-GENERATED.sh"
-}
-
-main "$@"
+while getopts "fvt:" OPTION
+do
+    case $OPTION in
+        v   ) VERBOSE=1;;
+        t   ) DOTFILES_TARGET="$OPTARG";;
+        *   ) fatal Unknown option chosen;;
+    esac
+done
+info Updating all submodules
+git submodule update --init
+info Autorunning all autorun scripts
+autorun_all
+info Autolinking all autolink files
+autolink_all
+info Re-sourcing bashrc
+. ~/.bashrc
