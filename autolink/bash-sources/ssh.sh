8585375bdb217f965ec2e8901ef6e371ebaedd08
Tony Wooster <twooster@gmail.com>
2016-06-05 10:15:26 -0400

    Some whitespace fixes, etc, in ssh bash-source

--- autolink/bash-sources/ssh.sh
+++ autolink/bash-sources/ssh.sh
@@ -7,17 +7,19 @@ apply_ssh() {
 }
 
 ssh-reagent () {
-        for agent in /tmp/ssh-*/agent.*; do
-                export SSH_AUTH_SOCK=$agent
-                if ssh-add -l 2>&1 > /dev/null; then
-                        echo Found working SSH Agent:
-                        ssh-add -l
-                        return
-                fi
-        done
-        echo Cannot find ssh agent - maybe you should reconnect and forward it?
+  for agent in /tmp/ssh-*/agent.*; do
+    export SSH_AUTH_SOCK=$agent
+    if ssh-add -l 2>&1 > /dev/null; then
+      echo Found working SSH Agent:
+      ssh-add -l
+      return
+    fi
+  done
+  echo Cannot find ssh agent - maybe you should reconnect and forward it?
 }
 
 # Tab completion for ssh hosts, from:
 #  http://feeds.macosxhints.com/~r/macosxhints/recent/~3/257065700/article.php
-[ -r "$HOME/.ssh/known_hosts" ] && complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh apply_ssh
+if [ -r "$HOME/.ssh/known_hosts" ]; then
+  complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh apply_ssh
+fi
