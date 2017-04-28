41ab5ff33bf0d0583b2a54cfcfa459206dc2fb09
Tony Wooster <twooster@gmail.com>
2017-04-28 20:21:53 +0200

    Add more openssl aliases

--- link/bash-sources/openssl.sh
+++ link/bash-sources/openssl.sh
@@ -1,3 +1,7 @@
-openssl-check-remote-cert() {
-  openssl s_client -showcerts -connect "$1"
+openssl-show-remote-cert() {
+  openssl s_client -showcerts -connect "$1:${2-443}"
+}
+
+openssl-check-remote-cert-expiration() {
+  openssl s_client -connect "$1:${2-443}" | openssl x509 -noout -dates
 }
