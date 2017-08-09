d8c4392e1fec138677fc32e5cbe2b2b783c1cbd7
Tony Wooster <twooster@gmail.com>
2017-08-09 12:48:41 +0200

    Fixup openssl aliases

--- link/bash-sources/openssl.sh
+++ link/bash-sources/openssl.sh
@@ -1,7 +1,7 @@
 openssl-show-remote-cert() {
-  openssl s_client -showcerts -connect "$1:${2-443}"
+  openssl s_client -showcerts -servername "$1" -connect "$1:${2-443}"
 }
 
 openssl-check-remote-cert-expiration() {
-  openssl s_client -connect "$1:${2-443}" | openssl x509 -noout -dates
+  openssl s_client -servername "$1" -connect "$1:${2-443}" | openssl x509 -noout -dates
 }
