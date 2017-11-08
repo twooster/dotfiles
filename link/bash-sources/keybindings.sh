13892748c446f62509ace5f55a5c4d68e79be956
Tony Wooster <twooster@gmail.com>
2017-11-09 00:33:39 +0100

    update keybindings, Ctrl-E now clears screen

--- link/bash-sources/keybindings.sh
+++ link/bash-sources/keybindings.sh
@@ -1,4 +1,6 @@
-bind '"\e\e[D": backward-word'
-bind '"\e\e[C": forward-word'
-bind '"\e\e[B": end-of-line'
-bind '"\e\e[A": beginning-of-line'
+#bind '"\e\e[D": backward-word'
+#bind '"\e\e[C": forward-word'
+#bind '"\e\e[B": end-of-line'
+#bind '"\e\e[A": beginning-of-line'
+# Ctrl-E:
+bind '"\005": clear-screen'
