ae5f34cb11f4e62ba859d69e62fa318474d7a779
Tony Wooster <twooster@gmail.com>
2018-07-18 00:50:44 +0200

    Update rg alias

--- link/_bash-sources/aliases.sh
+++ link/_bash-sources/aliases.sh
@@ -5,8 +5,11 @@ alias more="less"
 alias vimrc="vim ~/.vimrc"
 alias :q='exit'
 alias v='vim'
-alias rg="rg -M 200"
+alias rg="rg -M 200 -L"
 alias ack=rg
+alias o=xdg-open
 
 alias ..='cd ..'
 alias ...='cd ../..'
+
+alias k='kubectl'
