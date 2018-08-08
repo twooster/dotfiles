eae3ebe334329fc7a98baf011bf4cf72cb630c7e
Tony Wooster <twooster@gmail.com>
2018-08-08 16:42:21 +0200

    Fixup go stuff

--- link/_bash-sources/go.sh
+++ link/_bash-sources/go.sh
@@ -1,5 +1,8 @@
 if [ -d "${HOME}/.go/current" ] ; then
   export GOROOT="${HOME}/.go/current"
+fi
+
+if [ -d "${HOME}/go" ] ; then
   export GOPATH="${HOME}/go"
   export PATH="${PATH}:${GOPATH}/bin"
 fi
