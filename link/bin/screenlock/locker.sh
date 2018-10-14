8f4dd198ac0a1ffe44b5be198dfc3251823dd7b1
Tony Wooster <twooster@gmail.com>
2018-10-14 10:38:14 +0200

    Add screenlock scripts

---
+++ link/bin/screenlock/locker.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+IMG_FORMAT=png
+IMG_PATH="${XDG_RUNTIME_DIR:-${HOME}}/.sleepshot.${IMG_FORMAT}"
+
+INHIBIT_TIME=0.5
+
+BACKLIGHT_CMD="$HOME/bin/backlight"
+BACKLIGHT=100
+LOCKER_CMD=( i3lock --nofork --ignore-empty-password --color=000033 --image="${IMG_PATH}" )
+# Run before starting the locker
+pre_lock() {
+    BACKLIGHT=$( "${BACKLIGHT_CMD}" )
+    import -window root "${IMG_FORMAT}":- | mogrify -grayscale Rec709Luma -virtual-pixel mirror -blur 0x4 "${IMG_FORMAT}":- > "${IMG_PATH}"
+    backlight 30
+}
+
+# Run after the locker exits
+post_lock() {
+    if [ -f "${IMG_PATH}" ] ; then
+      rm "${IMG_PATH}"
+    fi
+    "${BACKLIGHT_CMD}" "${BACKLIGHT}"
+}
+
+###############################################################################
+
+pre_lock
+
+# kill locker if we get killed
+trap 'kill %%' TERM INT
+
+if [[ -e /dev/fd/${XSS_SLEEP_LOCK_FD:--1} ]]; then
+    # lock fd is open, make sure the locker does not inherit a copy
+    "${LOCKER_CMD[@]}" ${XSS_SLEEP_LOCK_FD}<&- &
+
+    sleep "${INHIBIT_TIME}"
+
+    # now close our fd (only remaining copy) to indicate we're ready to sleep
+    exec ${XSS_SLEEP_LOCK_FD}<&-
+else
+    "${LOCKER_CMD[@]}" &
+fi
+
+wait # for locker to exit
+
+post_lock
---
+++ link/bin/screenlock/notify.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+notify-send \
+  -u critical \
+  -i system-lock-screen \
+  -t 5000 \
+  -- '*** LOCKING SCREEN IN 30 SECS ***'
