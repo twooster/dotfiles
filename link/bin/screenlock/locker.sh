be0d27423291f0e315e9ae2d277ac98172f6235c
Tony Wooster <twooster@gmail.com>
2018-11-14 12:30:35 +0100

    Fix up screen locker backlight

--- link/bin/screenlock/locker.sh
+++ link/bin/screenlock/locker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 IMG_FORMAT=png
-IMG_PATH="${XDG_RUNTIME_DIR:-${HOME}}/.sleepshot.${IMG_FORMAT}"
+IMG_PATH="${XDG_RUNTIME_DIR:-${HOME}}/sleepshot.${IMG_FORMAT}"
 
 INHIBIT_TIME=0.5
 
@@ -10,30 +10,31 @@ BACKLIGHT=100
 LOCKER_CMD=( i3lock --nofork --ignore-empty-password --color=000033 --image="${IMG_PATH}" )
 # Run before starting the locker
 pre_lock() {
-    BACKLIGHT=$( "${BACKLIGHT_CMD}" )
+  BACKLIGHT=$( "${BACKLIGHT_CMD}" )
 
-    import \
-      -window root \
-      -colorspace Gray \
-      -filter gaussian \
-      -define filter:blur=12 \
-      -resize '100%' \
-      "${IMG_PATH}"
+  import \
+    -window root \
+    -colorspace Gray \
+    -filter gaussian \
+    -define filter:blur=12 \
+    -resize '100%' \
+    "${IMG_PATH}"
 
-    if [ "${BACKLIGHT}" -gt 20 ] ; then
-      "${BACKLIGHT_CMD}" 20
-    else
-      BACKLIGHT=
-    fi
+  if [ "${BACKLIGHT}" -gt 20 ] ; then
+    "${BACKLIGHT_CMD}" 20
+  else
+    BACKLIGHT=
+  fi
 }
 
 # Run after the locker exits
 post_lock() {
-  if [ -f "${IMG_PATH}" ] ; then
-    rm "${IMG_PATH}"
-  fi
   if [ -n "${BACKLIGHT}" ] ; then
     "${BACKLIGHT_CMD}" "${BACKLIGHT}"
+  fi
+
+  if [ -e "${IMG_PATH}" ] ; then
+    rm -f -- "${IMG_PATH}"
   fi
 }
 
@@ -44,16 +45,16 @@ pre_lock
 # kill locker if we get killed
 trap 'kill %%' TERM INT
 
-if [[ -e /dev/fd/${XSS_SLEEP_LOCK_FD:--1} ]]; then
-    # lock fd is open, make sure the locker does not inherit a copy
-    "${LOCKER_CMD[@]}" ${XSS_SLEEP_LOCK_FD}<&- &
+if [ -e /dev/fd/${XSS_SLEEP_LOCK_FD:--1} ] ; then
+  # lock fd is open, make sure the locker does not inherit a copy
+  "${LOCKER_CMD[@]}" ${XSS_SLEEP_LOCK_FD}<&- &
 
-    sleep "${INHIBIT_TIME}"
+  sleep "${INHIBIT_TIME}"
 
-    # now close our fd (only remaining copy) to indicate we're ready to sleep
-    exec ${XSS_SLEEP_LOCK_FD}<&-
+  # now close our fd (only remaining copy) to indicate we're ready to sleep
+  eval "exec ${XSS_SLEEP_LOCK_FD}<&-"
 else
-    "${LOCKER_CMD[@]}" &
+  "${LOCKER_CMD[@]}" &
 fi
 
 wait # for locker to exit
