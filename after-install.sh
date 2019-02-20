2a7934155781d91ac6fa1b0824434f0011b4764c
Tony Wooster <twooster@gmail.com>
2019-02-20 12:40:40 +0100

    Big push to XDG_ dirs

--- after-install.sh
+++ after-install.sh
@@ -1,1 +1,29 @@
-systemctl --user enable xrandr-on-wake
+#!/usr/bin/env bash
+: ${XDG_CACHE_HOME:="$HOME/.cache"}
+
+set -e
+set +x
+
+systemctl --user enable \
+  sleep.target \
+  watch-sleep.service \
+  xrandr-on-wake.service
+
+mkdir -p "$HOME/.cache"
+mkdir -p "$HOME/.local"
+mkdir -p "${XDG_CACHE_HOME}"/vim/{undo,swap,backup}
+
+
+which git || sudo apt install git
+which go || sudo apt install golang
+which fzf || {
+  git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME"/.local/fzf
+  cd "$HOME"/.local/fzf
+  ./install --no-fish --no-bash --no-zsh --xdg
+}
+which rustup || { curl https://sh.rustup.rs -sSf | sh -s ; }
+which ambr || cargo install amber
+which bat || cargo install bat
+which exa || cargo install exa
+which fd || cargo install fd-find
+which rg || cargo install ripgrep
---
+++ link/_config/fish/conf.d/n.fish
@@ -0,0 +1,2 @@
+set -x N_PREFIX $HOME/n
+set -g PATH $HOME/n/bin $PATH
