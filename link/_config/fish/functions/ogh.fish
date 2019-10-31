e581010229f586ce947dc4588df002735c6211b1
Tony Wooster <twooster@gmail.com>
2019-10-31 17:23:25 +0100

    Improve ogh to handle all variety of git repos, branch names

--- link/_config/fish/functions/ogh.fish
+++ link/_config/fish/functions/ogh.fish
@@ -6,19 +6,22 @@ function ogh
   end
 
   set remote (string replace -r "/.*" "" "$remote_branch")
-  set branch (string replace -r ".*/" "" "$remote_branch")
+  set branch (string sub -s (string length "$remote//") "$remote_branch")
   set remote_url (git remote get-url "$remote")
   switch "$remote_url"
-  case "http*"
+  case "https://github.com/*.git"
+    set remote_url (string sub -l (math (string length "$remote_url") - 4) "$remote_url")
+  case "https://github.com/*"
     # noop
+  case "git@github.com:*.git"
+    set remote_url "https://github.com/"(string sub -s 16 "$remote_url" -l (math (string length "$remote_url") - 19))
+  case "git@github.com:*"
+    set remote_url "https://github.com/"(string sub -s 16 "$remote_url")
   case "*"
-    set remote_url ( \
-      string replace -r \
-          '^([^@]+@)?([^:]+):(.*?)(.git)?$' \
-          'https://$2/$3/compare' \
-          "$remote_url"
-    )
-    set remote_url "$remote_url/$branch?expand=1"
+    echo "Remote URL does not seem to be GitHub-like: $remote_url"
+    return 1
   end
+
+  set remote_url "$remote_url/compare/$branch?expand=1"
   xdg-open "$remote_url"
 end
