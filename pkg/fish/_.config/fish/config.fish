d8ea2f02e572f4a214dbe841b2b287d8eb2f696f
Tony Wooster <twooster@gmail.com>
2021-05-19 18:21:10 +0200

    add kubectx

--- pkg/fish/_.config/fish/config.fish
+++ pkg/fish/_.config/fish/config.fish
@@ -4,5 +4,6 @@ if command -s direnv >/dev/null
   eval (direnv hook fish)
 end
 
+contains "$HOME/.local/bin" $fish_user_paths; or set -Ua fish_user_paths "$HOME/.local/bin"
 contains "$HOME/go/bin" $fish_user_paths; or set -Ua fish_user_paths "$HOME/go/bin"
 contains "$HOME/.cargo/bin" $fish_user_paths; or set -Ua fish_user_paths "$HOME/.cargo/bin"
